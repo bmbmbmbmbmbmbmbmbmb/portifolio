c47f160d09b54af674fa8581ea2950543ef1b789
@nicolygoulart <116736949+bmbmbmbmbmbmbmbmbmb@users.noreply.github.com>
2023-05-30 09:29:24 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+const menuhaburguer = document.querySelector(".menu-haburguer");
+const menu = document.querySelector(".menu");
+
+menuhaburguer.addEventListener('click', () => {
+
+    menu.classList.toggle('active');
+
+});
