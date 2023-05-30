2de8f5d6f1912062147ae6c45430aa3ec867c195
@nicolygoulart <116736949+bmbmbmbmbmbmbmbmbmb@users.noreply.github.com>
2023-05-30 09:37:05 -0300

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-const menuhaburguer = document.querySelector(".menu-haburguer");
-const menu = document.querySelector(".menu");
-
-menuhaburguer.addEventListener('click', () => {
-
-    menu.classList.toggle('active');
-
-});
